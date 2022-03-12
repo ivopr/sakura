e73c98169b2b70d53849dbcadbc9762cfa8dfc5c
Ivo <ivoprovensi1@gmail.com>
2022-03-12 15:25:13 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['mantine', 'plugin:@next/next/recommended'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
