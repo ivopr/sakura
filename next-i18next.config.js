eb47d18a56b3040b9326ce8137c594d6ef12f68e
Ivo <ivoprovensi1@gmail.com>
2022-03-15 08:51:47 -0400

    fix: app crashing in production

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+
 module.exports = {
   i18n: {
     locales: ["en", "pt"],
     defaultLocale: "en",
-    reloadOnPrerender: true,
+    localePath: path.resolve("./public/locales"),
+    reloadOnPrerender: process.env.NODE_ENV === "development",
   },
 };
